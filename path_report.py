f62eebcc038587617081e3e68c453d8c7c46a915
Ankhiver <ankhiver@gmail.com>
2026-03-27 11:15:44 +0100

    Added CLI path existence check

--- path_report.py
+++ path_report.py
@@ -1,11 +1,15 @@
-import pathlib
 import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser(
-    description="Small tool that reads path as an argument and prints it back"
+    description="Small tool that takes a path as an argument and prints it back"
 )
-parser.add_argument("path", help="Path to file or dierctory")
-
+parser.add_argument("path", help="Path to a file or directory")
 args = parser.parse_args()
 
-print(args.path)
+path = Path(args.path)
+
+print(path)
+print(path.exists())
+print(path.is_file())
+print(path.is_dir())
