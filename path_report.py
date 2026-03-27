6dc96a94ad81132b5834ca9209cff6c33f6520d9
Ankhiver <ankhiver@gmail.com>
2026-03-27 11:45:16 +0100

    Added file and directory report

--- path_report.py
+++ path_report.py
@@ -9,7 +9,15 @@ args = parser.parse_args()
 
 path = Path(args.path)
 
-print(path)
-print(path.exists())
-print(path.is_file())
-print(path.is_dir())
+if not path.exists():
+    print(f"Path object '{path}' doesn't exist")
+elif path.is_file():
+    print(f"Path object '{path}' is a file.")
+    print(f"File name: {path.name}")
+    print(f"File suffix: {path.suffix}")
+elif path.is_dir():
+    print(f"Path object '{path}' is a directory.")
+    items = list(path.iterdir())
+    print(f"Number of files in this directory: {len(items)}")
+else:
+    print("There is an error with argument passed to the tool.")
